74b134949372f156e5480a18e2a1f63121dc3657
REBECCA FRITH <rfrit@deakin.edu.au>
2017-04-30 12:32:28 +0930

    Edited Web.config / database connectivity to allow relative connection to the database (so it should work for everyone, hopefully!)

--- EmbrOnlineStore/EmbrOnlineStore/Controllers/Utilities/DatabaseFacilitator.cs
+++ EmbrOnlineStore/EmbrOnlineStore/Controllers/Utilities/DatabaseFacilitator.cs
@@ -9,9 +9,9 @@ namespace EmbrOnlineStore.Controllers.Utilities
 {
     public class DatabaseFacilitator 
     {
-        protected string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\pp2017tri1\Source\Repos\PracticalProjectT12017\EmbrOnlineStore\EmbrOnlineStore\App_Data\EmbrDB.mdf;Integrated Security=True";
+        protected string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["EMBRDB"].ConnectionString; //@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\pp2017tri1\Source\Repos\PracticalProjectT12017\EmbrOnlineStore\EmbrOnlineStore\App_Data\EmbrDB.mdf;Integrated Security=True";
 
-    
+
         private SqlConnection connection;
        
        public SqlConnection GetDatabaseConnection()
